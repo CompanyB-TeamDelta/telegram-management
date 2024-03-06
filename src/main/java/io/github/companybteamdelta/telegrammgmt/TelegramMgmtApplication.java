27a706994316703cfc6a7ade050f49336db850ea
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-03-06 21:44:43 +0200

    init

---
+++ src/main/java/io/github/companybteamdelta/telegrammgmt/TelegramMgmtApplication.java
@@ -0,0 +1,13 @@
+package io.github.companybteamdelta.telegrammgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/companybteamdelta/telegrammgmt/TelegramMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.companybteamdelta.telegrammgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
